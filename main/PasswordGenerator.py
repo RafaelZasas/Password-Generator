852b8f40abef31c64436932e3feecf6d0792cefd
Rafael Zasas <42390827+RafaelZasas@users.noreply.github.com>
2020-06-01 12:05:18 -0700

    Merging new features into development (#9)
    * Update
    
    Added a new Virtual Environment
    Added Doc Strings
    Fixed Errors
    Added runnable script from main
    
    * Added to gitignore

--- main/PasswordGenerator.py
+++ main/PasswordGenerator.py
@@ -2,26 +2,36 @@ import random
 import pandas as pd
 import random
 
+
 class PasswordGenerator:
+    """
+        Main Class
+        :method generate_password(): Main Method which creates and returns the password
+    """
     data_url = ''
 
-    def __init__(self, url):
-        self.data_url = url
+    def __init__(self):
+        pass
 
     @staticmethod
-    def generate_password(p_length=10, use_symbols=False):
+    def random_index(x):
+        """
+
+        :param x: to get random index from list so we want a value from 0 to (length-1)
+        :return: Random index to be used
+        """
+        return random.randint(0, x - 1)
+
+    def generate_password(self, p_length=10, use_symbols=False):
         """
         Creates a new 10 digit password, symbols optional.
 
-        :param
-            p_length (int): length of the password string created
-        :param
-            use_symbols (boolean): are symbols allowed in the string password
+        :param use_symbols: (Boolean) Flag whether to use symbols in password generation or not
+        :param p_length: (int) length of the password string created
+
         :return
             (string): returns unique string password
         """
-        def random_index(x):
-            return random.randint(0, x - 1) # This is to get a random index from a list so we want a value from 0 to (length-1)
 
         lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
                      'u', 'v', 'w', 'x', 'y', 'z']
@@ -29,29 +39,25 @@ class PasswordGenerator:
                      'U', 'V', 'W', 'X', 'Y', 'Z']
         numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
         symbols = ['!', '@', '#', '$', '%', '&', '?']
-        password = ''
+        pwd = ''
         # Decide whether we want symbols in the password
         if use_symbols:
             n = 4
         else:
             n = 3
         for i in range(p_length):
-            list_choice = random_int(n)  # choose a list to select from
+            list_choice = random.randint(0, n)  # choose a list to select from
 
             # From the chosen list pick a random character
             if list_choice == 0:
-                character = lowercase[random_index(len(lowercase))]
+                character = lowercase[self.random_index(len(lowercase))]
             elif list_choice == 1:
-                character = uppercase[random_index(len(uppercase))]
+                character = uppercase[self.random_index(len(uppercase))]
             elif list_choice == 2:
-                character = numbers[random_index(len(numbers))]
+                character = numbers[self.random_index(len(numbers))]
             elif list_choice == 3:
-                character = symbols[random_index(len(symbols))]
+                character = symbols[self.random_index(len(symbols))]
 
-            password += character  # add the random char from random list
+            pwd += character  # add the random char from random list
 
-        return password
-
-
-x = PasswordGenerator("x")
-print(x.generate_password(15, True))
+        return pwd
--- main/__main__.py
+++ main/__main__.py
@@ -1,2 +1,6 @@
+from main import PasswordGenerator
+
 if __name__ == '__main__':
-    print("hello world")
+    func = PasswordGenerator.PasswordGenerator()
+    pwd = func.generate_password()
+    print(f'Generated Password: {pwd}')
