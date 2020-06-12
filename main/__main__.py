9c14522e3af1dcd66b926cecd0c5132295b484fa
mattfisc <mattfisc@gmail.com>
2020-06-11 17:35:04 -0700

    for changes (#1)
    * api password (#13)
    
    * string doc by sphinx for password generator completed
    
    * rename url
    
    * notes
    
    * push test prac only
    
    * delete notes
    
    * Revert "Merge branch 'matt-dev' into matt-local-master"
    
    This reverts commit fe2d1fc81016e40c41eea58c444de7de88eb621a.
    
    * gitflow-feature-stash: api_password
    
    api passoword
    
    Co-authored-by: Rafael Zasas <42390827+RafaelZasas@users.noreply.github.com>
    Co-authored-by: Rafael Zasas <zasas.rafi@gmail.com>
    
    * Updated Git Ignore
    
    Co-authored-by: Rafael Zasas <42390827+RafaelZasas@users.noreply.github.com>
    Co-authored-by: Rafael Zasas <zasas.rafi@gmail.com>

--- main/__main__.py
+++
@@ -1,6 +0,0 @@
-from main import PasswordGenerator
-
-if __name__ == '__main__':
-    func = PasswordGenerator.PasswordGenerator()
-    pwd = func.generate_password()
-    print(f'Generated Password: {pwd}')
