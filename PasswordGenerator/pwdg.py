34064c606a30230eacabc4644cfc36875438aabd
Rafael Zasas <zasas.rafi@gmail.com>
2020-07-02 10:26:33 -0700

    Changed pwdg Logic
    * Created a dictionary of lists instead of having lists separate.
    
    * Randomly selects dict key and then char from the randomly chosen list.
    
    * All unit tests still work.

--- PasswordGenerator/pwdg.py
+++ PasswordGenerator/pwdg.py
@@ -1,6 +1,5 @@
 import random
-import pandas as pd
-import random
+import numpy as np
 
 
 class PasswordGenerator:
@@ -8,21 +7,12 @@ class PasswordGenerator:
         Main Class
         :method generate_password(): Main Method which creates and returns the password
     """
-    data_url = ''
 
     def __init__(self):
         pass
 
     @staticmethod
-    def random_index(x):
-        """
-
-        :param x: to get random index from list so we want a value from 0 to (length-1)
-        :return: Random index to be used
-        """
-        return random.randint(0, x - 1)
-
-    def generate_password(self, p_length, use_symbols):
+    def generate_password(p_length=10, use_symbols=False):
         """
         Creates a new 10 digit password, symbols optional.
         :param p_length: (int) length of the password to be created.
@@ -31,31 +21,23 @@ class PasswordGenerator:
             (string): unique string password.
         """
 
-        lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-                     'u', 'v', 'w', 'x', 'y', 'z']
-        uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-                     'U', 'V', 'W', 'X', 'Y', 'Z']
-        numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-        symbols = ['!', '@', '#', '$', '%', '&', '?']
+        characters = {
+            'lowercase': ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                          't', 'u', 'v', 'w', 'x', 'y', 'z'],
+            'uppercase': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+                          'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+            'numbers': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+            'symbols': ['!', '@', '#', '$', '%', '&', '?']
+        }
+
         pwd = ''
-        # Decide whether we want symbols in the password
-        if use_symbols:
-            n = 3
-        else:
-            n = 2
-        for i in range(p_length):
-            list_choice = random.randint(0, n)  # choose a list to select from
 
-            # From the chosen list pick a random character
-            if list_choice == 0:
-                character = lowercase[self.random_index(len(lowercase))]
-            elif list_choice == 1:
-                character = uppercase[self.random_index(len(uppercase))]
-            elif list_choice == 2:
-                character = numbers[self.random_index(len(numbers))]
-            elif list_choice == 3:
-                character = symbols[self.random_index(len(symbols))]
+        for i in range(p_length):  # one iteration per password char
+            if use_symbols:
+                list_choice = random.choice(list(characters))  # choose a list randomly from the dict keys
+            else:
+                list_choice = random.choice(list(characters)[:-1])  # dont include symbols list
 
-            pwd += character  # add the random char from random list
+            pwd += random.choice(list(characters[list_choice]))  # append a random char from the chosen list to pwd
 
         return pwd
