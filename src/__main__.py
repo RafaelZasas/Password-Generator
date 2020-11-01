38d43c17865b6f53236b27059e8dfca4098f0f88
Rafael Zasas <zasas.rafi@gmail.com>
2020-10-31 21:18:27 -0800

    More Options 🚀
    ✨ Added options to have either letters, numbers, symbols or a combination
    ✅ Added Unit Testing for new options

--- src/__main__.py
+++ src/__main__.py
@@ -2,5 +2,5 @@ import PasswordGenerator
 
 if __name__ == '__main__':
     func = PasswordGenerator.pwdg.PasswordGenerator()
-    pwd = func.generate_password(10, True)
+    pwd = func.generate_password(10, True, True, False)
     print(f'Generated Password: {pwd}')
